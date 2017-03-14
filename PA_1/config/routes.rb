313f0f060da08db58d22eb664ac9589beaaa5f60
Aaron Gold <aharonhillel@brandeis.edu>
2017-03-14 18:14:32 -0400

    added login sessions and if statements to nav

--- PA_1/config/routes.rb
+++ PA_1/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   resources :users
@@ -6,6 +8,9 @@ Rails.application.routes.draw do
   resources :courses
   resources :subjects
   root 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
