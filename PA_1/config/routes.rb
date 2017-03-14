8cbbf0a6d261c5febb37ad3e0af55a16aeee7bbd
Aaron Gold <aharonhillel@brandeis.edu>
2017-03-14 17:30:05 -0400

    Added login page

--- PA_1/config/routes.rb
+++ PA_1/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :instructors
   resources :courses
   resources :subjects
-  root 'users#index'
+  root 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
