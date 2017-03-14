768e65422f632449eed3d4420bff97ef94d94813
Aaron Gold <aharonhillel@brandeis.edu>
2017-03-14 19:36:57 -0400

    Added Helper for Classes

--- PA_1/app/helpers/courses_helper.rb
+++ PA_1/app/helpers/courses_helper.rb
@@ -1,2 +1,25 @@
 module CoursesHelper
+
+  def display_loop(courses)
+string = ""
+  @courses.each do |course|
+     string + <<-eos "<div class='row individual_course'>"
+    link_to '#{course.name}', course
+         "<br/>"
+         "<div class='col-md-10 offset-md-2'>"
+         #{course.description}
+         -
+         #{course.code}
+         string << "</div>  <div class='col-md-10 offset-md-2'> Independent Study:"
+         eos
+
+  if course.independent_study == true
+  string + string<< "link_to fa_icon('check')" #<!-- font awesome -->
+   else
+    string + string << "link_to fa_icon('times')"
+   end
+  string + string << " </div> </div>"
+    end
+     string.html_safe
+  end
 end
