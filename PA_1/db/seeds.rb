b3196c797d8791ddae64c9096dd49c817072f31a
Aaron Gold <aharonhillel@brandeis.edu>
2017-03-14 00:47:45 -0400

    added all seed files

--- PA_1/db/seeds.rb
+++ PA_1/db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+def seed_courses
+	json_seed = JSON.parse(File.read('db/json_seed/course.json'))
+
+	json_seed.each do |c|
+		Course.create(independent_study: c["independent_study"], name: c["name"],  code: c["code"],  credits: c["credits".to_i], description: c["description"])
+	end
+end
+
+def seed_instructor
+	json_seed = JSON.parse(File.read('db/json_seed/instructor.json'))
+
+	json_seed.each do |instructor|
+		Instructor.create(first_name: instructor["first"], middle_name: instructor["middle"],lsat_name: instructor["last"], email: instructor["email"])
+	end
+end
+def seed_subject
+	json_seed = JSON.parse(File.read('db/json_seed/subject.json'))
+
+	json_seed.each do |subject|
+		Subject.create(name: subject["name"])
+	end
+end
+def auto_clear_tables
+  # I added this so I don't always have to do db:drop:all
+  Course.delete_all
+  Instructor.delete_all
+  Subject.delete_all
+end
+auto_clear_tables
+seed_courses
+seed_instructor
+seed_subject
